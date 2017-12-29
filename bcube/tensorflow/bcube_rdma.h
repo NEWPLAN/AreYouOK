9df30ac0f7b2fbb9456f99bc4317c3ffba90106c
NEWPLAN <enwplan001@163.com>
2017-12-29 10:48:06 +0800

    rdma setup.py

--- bcube/tensorflow/bcube_rdma.h
+++ bcube/tensorflow/bcube_rdma.h
@@ -67,6 +67,8 @@ struct _recv_chain
 	void* data_ptr;
 	_recv_chain* next;
 };
+struct bcube_struct;
+//struct tensor_table_entry;
 bool rdma_all_init(bcube_struct& bcube_s);
 bool bcube_send_by_rdma(tensor_table_entry& e, bcube_struct& bs, int stage);
 
