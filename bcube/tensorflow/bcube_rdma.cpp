a5c1092ddac1f6fd2db6173214c6218612c23490
NEWPLAN <enwplan001@163.com>
2017-12-31 16:16:03 +0800

    rdma test

--- bcube/tensorflow/bcube_rdma.cpp
+++ bcube/tensorflow/bcube_rdma.cpp
@@ -316,7 +316,7 @@ static node_item* send_by_RDMA(struct ibv_wc *wc, node_item* nit)
 #if __RDMA_SLOW__send
 			printf("thread %ld will send data %lp in 1 seconds\n", pthread_self(), nit);
 
-			std::this_thread::sleep_for(std::chrono::millseconds(1));
+			std::this_thread::sleep_for(std::chrono::milliseconds(1));
 #endif
 
 #if _SEND_REAL_DATA_
@@ -342,11 +342,11 @@ static node_item* send_by_RDMA(struct ibv_wc *wc, node_item* nit)
 			ctx->remote_idle = true;
 #if __RDMA_SLOW__send
 			printf("thread %ld will send data %lp in 10 seconds\n", pthread_self(), nit);
-			std::this_thread::sleep_for(std::chrono::millseconds(1));
+			std::this_thread::sleep_for(std::chrono::milliseconds(1));
 #endif
 #if _SEND_REAL_DATA_
 			while (nit->next == nullptr)
-				std::this_thread::sleep_for(std::chrono::millseconds(1));
+				std::this_thread::sleep_for(std::chrono::milliseconds(1));
 			node_item* next_node = nit->next;
 			std::free(nit);
 			nit = next_node;
@@ -630,7 +630,7 @@ static void recv_RDMA(bcube_global_struct& bgs)
 	{
 #if __RDMA_SLOW__
 		//printf("in recv loops will sleep for 1 seconds\n");
-		std::this_thread::sleep_for(std::chrono::millseconds(1));
+		std::this_thread::sleep_for(std::chrono::milliseconds(1));
 #endif
 		for (auto& recv_list : recv_chain)
 		{
