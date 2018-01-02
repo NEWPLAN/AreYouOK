3d476674d8cfe454981e7b66f4ec46d6f3d4158d
NEWPLAN <enwplan001@163.com>
2018-01-02 19:16:48 +0800

    rrrr

--- examples/tensorflow_mnist.py
+++ examples/tensorflow_mnist.py
@@ -60,7 +60,7 @@ def conv_model(feature, target, mode):
     # Second conv layer will compute 64 features for each 5x5 patch.
     with tf.variable_scope('conv_layer2'):
         h_conv2 = layers.conv2d(
-            h_pool1_3, 512, kernel_size=[5, 5], activation_fn=tf.nn.relu)
+            h_pool1_3, 64, kernel_size=[5, 5], activation_fn=tf.nn.relu)
         h_pool2 = tf.nn.max_pool(
             h_conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
         # reshape tensor into a batch of vectors
