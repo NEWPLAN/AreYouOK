a516d5c564e4ef47a282041a7ac833c5662a4610
NEWPLAN <enwplan001@163.com>
2018-01-02 19:22:43 +0800

    rrrr

--- examples/tensorflow_mnist.py
+++ examples/tensorflow_mnist.py
@@ -38,25 +38,6 @@ def conv_model(feature, target, mode):
             feature, 32, kernel_size=[5, 5], activation_fn=tf.nn.relu)
         h_pool1 = tf.nn.max_pool(
             h_conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-'''
-    with tf.variable_scope('conv_layer1_1'):
-        h_conv1_1 = layers.conv2d(
-            h_pool1, 64, kernel_size=[5, 5], activation_fn=tf.nn.relu)
-        h_pool1_1 = tf.nn.max_pool(
-            h_conv1_1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-
-    with tf.variable_scope('conv_layer1_2'):
-        h_conv1_2 = layers.conv2d(
-            h_pool1_1, 128, kernel_size=[5, 5], activation_fn=tf.nn.relu)
-        h_pool1_2 = tf.nn.max_pool(
-            h_conv1_2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-
-    with tf.variable_scope('conv_layer1_3'):
-        h_conv1_3 = layers.conv2d(
-            h_pool1_2, 32, kernel_size=[5, 5], activation_fn=tf.nn.relu)
-        h_pool1_3 = tf.nn.max_pool(
-            h_conv1_3, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-'''
     # Second conv layer will compute 64 features for each 5x5 patch.
     with tf.variable_scope('conv_layer2'):
         h_conv2 = layers.conv2d(
