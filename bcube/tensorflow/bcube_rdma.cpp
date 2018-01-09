6adc4c5bd8152d0e67c08c40edbd1b536bf861f2
NEWPLAN <enwplan001@163.com>
2018-01-09 14:22:21 +0800

    purge rdma concurrent

--- bcube/tensorflow/bcube_rdma.cpp
+++ bcube/tensorflow/bcube_rdma.cpp
@@ -503,7 +503,7 @@ static void _build_params(struct rdma_conn_param *params)
 {
 	memset(params, 0, sizeof(*params));
 
-	params->initiator_depth = params->responder_resources = MAX_CONCURRENCY;
+	params->initiator_depth = params->responder_resources = 1;
 	params->rnr_retry_count = 7; /* infinite retry */
 	params->retry_count = 7;
 }
