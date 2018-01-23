e8b5e0691dc497e3307065c3d23ae64d2f6e98e3
NEWPLAN <enwplan001@163.com>
2018-01-23 16:37:07 +0800

    are you ok

---
+++ Makefile
@@ -0,0 +1,32 @@
+# ==============================================================================
+# Copyright 2017 NEWPLAN. All Rights Reserved.
+#
+# Just for fun :).
+#
+# ==============================================================================
+CC = g++  
+WARNING=  -Wall
+CFLAGS += $(WARNING) -g  -std=c++11 -Os
+LDFLAGS += -lm  -pthread
+PROC_NAME += areyouok
+HEAD_DEPEND += 
+SOURCE +=  areyouok,cpp
+OBJS_DEPEND=$(subst .cpp,.o, $(SOURCE))
+all: clean $(PROC_NAME)
+	@echo 'make done'
+
+# genetate objs
+%.o:%.cpp $(HEAD_DEPEND)
+	$(CC) -c $(CFLAGS) $< -o $@
+
+$(PROC_NAME):$(OBJS_DEPEND)
+	$(CC) $^ -o $@ $(LDFLAGS)
+
+run:all
+	./$(PROC_NAME)
+
+.PHONY:clean
+clean:  
+	rm -rf $(OBJS_DEPEND) 
+	rm -rf ${PROC_NAME}
+
---
+++ areyouok.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+bool question(std::string ques)
+{
+	std::string u_ans;
+	std::cout << ques << std::endl;
+	std::cin >> u_ans;
+	if (u_ans != "yes")
+		return true;
+	return false;
+}
+
+void enqueue_ques(std::vector<std::string>& ques_pool)
+{
+	ques_pool.push_back("Are you OK?");
+	ques_pool.push_back("Do you like Mi4i?");
+	ques_pool.push_back("I'm very happy to be in China (emm, in Indina)..., do you like me?");
+	ques_pool.push_back("I will give you everybody a gift, a Mi band, are you happy?");
+}
+
+int main(int argc, char** argv)
+{
+	bool res;
+	std::vector<std::string> ques_pool;
+
+	enqueue_ques(ques_pool);
+
+	for (auto& it : ques_pool)
+	{
+		if (question(it))goto exit_fina;
+	}
+
+	std::cout << "You are a good man..., Bye." << std::endl;
+exit_fina:
+	std::cout << "Your answer is not my expected..., leaving you now!" << std::endl;
+	return -1;
+}
